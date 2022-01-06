04344be8dd62a21eeeec1cd112d9a1bd3abc8502
minthing <minjeongandk@gmail.com>
2022-01-06 21:37:31 +0900

    djangogirls tutorial 01
    * setting
    * models

---
+++ blog/myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/minjeong/Documents/GitHub/djangogirls_tutorial/blog/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
